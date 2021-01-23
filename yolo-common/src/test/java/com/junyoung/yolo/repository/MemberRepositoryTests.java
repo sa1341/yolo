e8bcd4711d0ed5d45f43639a500db7ffa67fc4fa
임준영 <a79007714@gmail.com>
2021-01-23 23:25:59 +0900

    query dsl 추가, 테스트 코드 작 - querydsl 및 Exception 테스트 수행성, database - docker mariadb로 변경

--- yolo-common/src/test/java/com/junyoung/yolo/repository/MemberRepositoryTests.java
+++ yolo-common/src/test/java/com/junyoung/yolo/repository/MemberRepositoryTests.java
@@ -1,13 +1,19 @@
 package com.junyoung.yolo.repository;
 
 import com.junyoung.yolo.domain.member.entity.Member;
-import com.junyoung.yolo.domain.member.repository.MemberRepository;
+import com.junyoung.yolo.domain.member.entity.QMember;
+import com.junyoung.yolo.exception.MemberNotFoundException;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,22 +24,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MemberRepositoryTests {
 
     @Autowired
-    private MemberRepository memberRepository;
+    private EntityManager em;
+
+    private JPAQueryFactory queryFactory;
+
+    @Before
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Member member1 = Member.create("junyoung", 30);
+        Member member2 = Member.create("minwan", 30);
+        Member member3 = Member.create("seungtop", 30);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+    }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
     @Test
-    public void saveMember() throws Exception {
+    public void saveMember() {
+        QMember member = QMember.member;
 
-        //given
-        Member member = Member.builder()
-                            .name("junyoung")
-                            .age(31)
-                            .build();
-        //when
-        memberRepository.save(member);
+        Member findMember = queryFactory
+                    .select(member)
+                    .from(member)
+                    .where(member.name.eq("11"))
+                    .fetchOne();
 
-        System.out.println("memberId: " + member.getId());
-
-        Member findMember = memberRepository.findById(member.getId()).get();
+        exceptionRule.expect(MemberNotFoundException.class);
+        exceptionRule.expectMessage("Member is not exist");
+        if (findMember == null) throw new MemberNotFoundException("Member is not exist");
 
         //then
         assertThat("junyoung").isEqualTo(findMember.getName());
--- yolo-web/src/main/java/com/junyoung/yolo/controller/IndexController.java
+++ yolo-web/src/main/java/com/junyoung/yolo/controller/IndexController.java
@@ -2,21 +2,24 @@ package com.junyoung.yolo.controller;
 
 
 import com.junyoung.yolo.domain.member.entity.Member;
+import com.junyoung.yolo.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class IndexController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    //private final MemberService memberService;
+    private final MemberService memberService;
 
     @GetMapping(value = "/index")
     public Member index() {
         Member member = Member.create("junyoung", 30);
-        //memberService.saveMember(member);
+        memberService.saveMember(member);
         logger.debug("name: {}", member.getName());
         logger.debug("age: {}", member.getAge());
         return member;
